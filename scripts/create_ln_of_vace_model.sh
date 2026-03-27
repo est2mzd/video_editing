baa45ddcd3da179bcaef4ddfea41b72d4c4c00da
est2mzd <est2mzd@gmail.com>
2026-03-27 17:18:13 +0000

    before do vace demo-2

---
+++ scripts/create_ln_of_vace_model.sh
@@ -0,0 +1,3 @@
+cd /workspace/third_party/VACE/models
+
+ln -s VACE-Wan2.1-1.3B-Preview Wan2.1-VACE-1.3B
---
+++ scripts/run_vace_demo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e 
+
+cd /workspace/third_party/VACE
+python3 vace/gradios/vace_wan_demo.py 
